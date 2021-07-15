c70aadc4d9df351e9335d1c1e833c2520da71302
Wilhelm <Wilya3@yandex.ru>
2021-07-15 14:25:27 +0600

    Initial commit

---
+++ site_parser/src/main/java/me/brinster/wilhelm/site_parser/SiteParser.java
@@ -0,0 +1,10 @@
+package me.brinster.wilhelm.site_parser;
+
+public class SiteParser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ site_parser/src/test/java/me/brinster/wilhelm/site_parser/test/SiteParserTest.java
@@ -0,0 +1,14 @@
+package me.brinster.wilhelm.site_parser.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SiteParserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
